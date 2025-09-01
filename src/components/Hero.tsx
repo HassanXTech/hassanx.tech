47c3e6d55c578839d679dd898a49d48dfbb67743
Hassan Tech <mysocialmedia755@gmail.com>
2025-08-31 21:37:42 -0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,7 @@ export default function Hero() {
           </p>
         </AnimatedSection>
         <AnimatedSection className="flex gap-3">
-          <a target="_blank" className="w-full md:w-auto" href="mailto:hsnshafique090@gmail.com">
+          <a target="_blank" className="w-full md:w-auto" href="mailto:hassantech.contact@gmail.com">
             <button className="inline-flex justify-center items-center gap-2.5 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 bg-zinc-900 text-white hover:bg-zinc-800 focus:ring-zinc-500 px-6 py-3.5 text-base rounded-2xl w-full md:w-auto">
               Hire me
             </button>
