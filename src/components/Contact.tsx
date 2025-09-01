e398655dc86a1e1c9d5000f0ba6e5adf68cbdb42
Hassan Tech <mysocialmedia755@gmail.com>
2025-08-31 21:34:17 -0700

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -15,7 +15,7 @@ export default function Contact() {
       </AnimatedSection>
       <AnimatedSection delay={0.1}>
         <div className="flex md:flex-row flex-col justify-center md:justify-start items-center gap-3">
-          <a target="_blank" className="w-full md:w-auto" href="mailto:hsnshafique090@gmail.com">
+          <a target="_blank" className="w-full md:w-auto" href="mailto:hassantech.contact@gmail.com">
             <button
               className="inline-flex justify-center items-center gap-2.5 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 bg-zinc-900 text-white hover:bg-zinc-800 focus:ring-zinc-500 px-5 py-3 text-sm rounded-xl w-full md:w-auto"
               type="button"
